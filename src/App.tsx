5d18f46c07b7ecd1adfe4615c0cd5f93ecf950bd
Zach Hamlin <zachhamlin6@gmail.com>
2019-04-08 19:37:10 -0500

    playing around with css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
 
 import 'bulma/css/bulma.css';
+import './App.css';
 
 class App extends React.Component {
   public render() {
     return (
-    <div className="columns is-centered">
-        <div className="column is-half" style={{paddingTop: '1%'}}>
-            <div className="box is-primary">
-                <article className="media">
-                    <h1 className="title">
-                        Hello World
-                    </h1>
-                </article>
+    <div className="ui">
+        <nav className="navbar app is-info">
+            <a className="navbar-item">
+              Home
+            </a>
+        </nav>
+        <nav className="navbar board is-warning">board choice here</nav>
+        <div className="lists is-warning">
+            <div className="list">
+                <ul>
+                    <li>l1</li>
+                    <li>l2</li>
+                    <li>l3</li>
+                </ul>
             </div>
         </div>
     </div>
