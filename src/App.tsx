2519699d3f13d5caef6e6767a977fd3dccbc3fcb
Zach Hamlin <zachhamlin6@gmail.com>
2019-04-09 17:20:36 -0500

    updated css to be trello like

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import * as React from 'react';
 
 import 'bulma/css/bulma.css';
-import './App.css';
+import './App.css'
 
 class App extends React.Component {
   public render() {
     return (
     <div className="ui">
-        <nav className="navbar app is-info">
+        <nav className="navbar app is-danger">
             <a className="navbar-item">
               Home
             </a>
         </nav>
-        <nav className="navbar board is-warning">board choice here</nav>
-        <div className="lists is-warning">
+        <nav className="navbar board">board choice here</nav>
+        <div className="lists">
             <div className="list">
-                <ul>
-                    <li>l1</li>
-                    <li>l2</li>
-                    <li>l3</li>
+                <h3 className="list-title">List title</h3>
+                <ul className="list-items">
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
+                    <li> item1 </li>
                 </ul>
+                <a className="list-button button is-light">Add card</a>
             </div>
         </div>
     </div>
